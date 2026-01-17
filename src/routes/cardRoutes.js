b13e287d1643a917d052daf73c6172fc08c024b5
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2026-01-17 20:55:21 +0100

    Update summary route to user

--- src/routes/cardRoutes.js
+++ src/routes/cardRoutes.js
@@ -4,6 +4,7 @@ import * as cardController from '../controllers/cardController.js';
 const router = express.Router();
 
 router.get('/summary/:username', cardController.getSummary);
+router.get('/user/:username', cardController.getSummary);
 router.get('/projects/:username', cardController.getProjects);
 
 export default router;
