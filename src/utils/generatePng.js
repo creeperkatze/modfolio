71c37a70b5c58d5f67ede82eb59b339491a507cf
Creeperkatze <178587183+Creeperkatze@users.noreply.github.com>
2026-01-21 15:40:00 +0100

    Revert "Fix font importing"
    This reverts commit 9da8de5074db45d1e3beb4d1c1f1d332877d2b5c.

--- src/utils/generatePng.js
+++ src/utils/generatePng.js
@@ -1,17 +1,15 @@
 import path from "path";
 import { Resvg } from "@resvg/resvg-js";
 import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
+import logger from "./logger.js";
 
-// Get the directory of the current module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// Load Inter font from public/fonts
+const fontPath = path.join(process.cwd(), "public", "fonts", "inter.ttf");
+const fontBuffer = readFileSync(fontPath);
 
 export async function generatePng(svgString)
 {
-    // Load font from a path relative to this file
-    const fontPath = path.join(__dirname, "..", "..", "public", "fonts", "inter.ttf");
-    const fontBuffer = readFileSync(fontPath);
+    logger.info(fontPath);
 
     const options = {
         fitTo: {
